f23587acb695b3564585eac4d482fd476027b511
YIqi17 <42971110+YIqi17@users.noreply.github.com>
2018-10-23 06:33:31 -0700

    Update 06/js/main.js

--- 06/js/main.js
+++ 06/js/main.js
@@ -8,10 +8,10 @@
 const messagewrapper = document.getElementById('message');
 
 
-const one = prompt('Enter your first number');
-const two = prompt('Enter your second number');
-const three = prompt('Enter your third number');
-const four = prompt('Enter your forth number')
+const one = parseInt(prompt('Enter your first number'));
+const two = parseInt(prompt('Enter your second number'));
+const three = parseInt(prompt('Enter your third number'));
+const four = parseInt(prompt('Enter your forth number'));
 
 let message = 'Your equation is ' + (one+two-three)*four;
 
