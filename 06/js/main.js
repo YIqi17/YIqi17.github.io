32eb04b688053ffceb59aa5112ce041147544f5b
YIqi17 <42971110+YIqi17@users.noreply.github.com>
2018-10-23 06:28:11 -0700

    Update 06/js/main.js

--- 06/js/main.js
+++ 06/js/main.js
@@ -8,38 +8,12 @@
 const messagewrapper = document.getElementById('message');
 
 
-const plus = prompt('56+177=?');
-const minus = prompt('233-35=?');
-const times = prompt('198x2=?');
-const total = prompt('(56+177-35)x2=?')
+const one = prompt('Enter your first number');
+const two = prompt('Enter your second number');
+const three = prompt('Enter your third number');
+const four = prompt('Enter your forth number')
 
-let message = '56+177=' + plus + '!\n';
-if (plus > 233) {
-    message += 'Wrong answer!\n';
-} else if (plus < 233) {
-    message += 'Wrong answer, please try again!\n';
-}
-
-message += '233-35=' + minus + '!\n';
-if (minus > 198) {
-    message += 'Wrong answer!\n';
-} else if (minus < 198) {
-    message += 'Wrong answer, please try again!\n';
-}
-
-message += '198x2=' + times + '!\n';
-if (times > 396) {
-    message += 'Wrong answer!\n';
-} else if (times < 396) {
-    message += 'Wrong answer, please try again!\n';
-}
-
-message += '(56+177-35)x2=' + total + '!\n';
-if (total > 396) {
-    message += 'Wrong answer!\n';
-} else if (total < 396) {
-    message += 'Wrong answer, please try again!\n';
-}
+let message = 'Your equation is ' + (one+two-three)*four;
 
 messagewrapper.innerText = message;
 
